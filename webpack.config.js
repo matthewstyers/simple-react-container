3cb3007f74f9d6d2c87b74dc7f04e11c678fa8ff
Matthew <matthew@abstractmediaco.com>
2016-03-15 13:08:21 -0500

    updates webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,71 @@
 const path = require('path');
+const webpack = require('webpack');
+
+function getEntrySources(sources) {
+  if (process.env.NODE_ENV !== 'production') {
+    sources.push('webpack-hot-middleware/client');
+  }
+
+  return sources;
+}
 
 module.exports = {
-  // devServer: {
-  //   historyApiFallback: true,
-  //   contentBase: './'
-  // },
-  entry: [
-    './src/index.js'
-  ],
+  devtool: process.env.NODE_ENV !== 'production' ? 'eval-source-map' : '',
+  entry: {
+    bundle: getEntrySources(['./src/index']),
+  },
   output: {
+    publicPath: '/dist/',
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist'),
-    publicPath: '/dist/'
   },
+  plugins: process.env.NODE_ENV !== 'production' ?
+    [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+    ] :
+    [
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          warnings: false,
+        },
+      }),
+    ],
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader!cssnext-loader',
+      },
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel-loader?stage=0', 'eslint-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        loader: 'url-loader?prefix=img/&limit=5000',
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot)$/,
+        loader: 'url-loader?prefix=font/&limit=5000',
+      },
+    ],
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  }
 };
